1a7df2ac52fb3a80a7e414c805bc8e3130e5729b
hossamahossam810-sketchhossam-reelify <hossamahossam810@gmail.com>
2026-08-30 22:56:38 +0300

    Create whop-sdk.ts

---
+++ lib/whop-sdk.ts
@@ -0,0 +1,8 @@
+import { WhopServerSdk } from "@whop/api";
+
+export const whopSdk = WhopServerSdk({
+  appId: process.env.NEXT_PUBLIC_WHOP_APP_ID!,
+  appApiKey: process.env.WHOP_API_KEY!,
+  onBehalfOfUserId: process.env.NEXT_PUBLIC_WHOP_AGENT_USER_ID,
+  companyId: process.env.NEXT_PUBLIC_WHOP_COMPANY_ID,
+});
